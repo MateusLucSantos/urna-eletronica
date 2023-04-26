c88378577fd2e5a5e2eb2c901f38f21fb2a66c85
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2023-04-25 21:28:02 -0300

    Projeto urna

---
+++ js/etapas.js
@@ -0,0 +1,72 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: "Fulano de Tal",
+                partido: "ABC",
+                fotos: [
+                    {
+                        url: "./images/coiote.png",
+                        legenda: "Vereador",
+                        small: false,
+                    },
+                ],
+            },
+            {
+                numero: '77222',
+                nome: "Beltrano da Silva",
+                partido: "DEFG",
+                fotos: [
+                    {
+                        url: "./images/galo.png",
+                        legenda: "Vereador",
+                        small: false,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: "Ciclano",
+                partido: "ABC",
+                vice: "Cic",
+                fotos: [
+                    {
+                        url: "./images/pernalonga.png",
+                        legenda: "Prefeito",
+                    },
+                    {
+                        url: "./images/papaleguas.png",
+                        legenda: "Vice-Prefeito",
+                        small: true,
+                    },
+                ],
+            },
+            {
+                numero: '84',
+                nome: "Zulano",
+                partido: "QEC",
+                vice: "Zul",
+                fotos: [
+                    {
+                        url: "./images/patolino.png",
+                        legenda: "Prefeito",
+                    },
+                    {
+                        url: "./images/frajola.png",
+                        legenda: "Vice-Prefeito",
+                        small: true,
+                    },
+                ],
+            },
+        ],
+    },
+];
